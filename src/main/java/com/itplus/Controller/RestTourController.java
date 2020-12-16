99927cc86db277e7dd40ecba1788ad96af2ec339
S2kael <laiducminh1002@gmail.com>
2020-12-17 03:35:09 +0700

    [Tour] merge url api search to getAll

--- src/main/java/com/itplus/Controller/RestTourController.java
+++ src/main/java/com/itplus/Controller/RestTourController.java
@@ -2,10 +2,9 @@ package com.itplus.Controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.sonatype.inject.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,8 +22,13 @@ public class RestTourController {
 	ObjectMapper objectMapper;
 
 	@GetMapping(value = "/")
-	public String listAllTour(HttpServletRequest request) throws JsonProcessingException {
-		List<TourDTO> list = tourService.getAllTour();
+	public String listAllTour(@Nullable @RequestParam String q) throws JsonProcessingException {
+		List<TourDTO> list;
+		if (q != null) {
+			list = tourService.search(q);
+		}else{
+			list = tourService.getAllTour();
+		}
 		return objectMapper.writeValueAsString(list);
 	}
 
@@ -37,12 +41,6 @@ public class RestTourController {
 	@GetMapping(value = "/{id}/")
 	public String listTourById(@PathVariable int id) throws JsonProcessingException {
 		TourDTO list = tourService.getTourById(id);
-		return objectMapper.writeValueAsString(list);
-	}
-
-	@GetMapping(value = "/search/")
-	public String listSearch(@RequestParam String q) throws JsonProcessingException {
-		List<TourDTO> list = tourService.search(q);
 		return objectMapper.writeValueAsString(list);
 	}
 }
